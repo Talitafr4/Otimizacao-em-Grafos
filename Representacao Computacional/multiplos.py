f967b5b000cbfd91cea27d67557168d0f9008134
Talitafr4 <ribeiro.talita@graduacao.uerj.br>
2024-04-01 17:40:02 -0300

    alguns ajustes

--- Representacao Computacional/multiplos.py
+++ Representacao Computacional/multiplos.py
@@ -1,1 +1,15 @@
-import grafos
+from Grafo import GrafoListaAdj
+
+G = GrafoListaAdj()
+n = input(int())
+G.DefinirN(n)
+while(True):
+    a, b = input().split(" ")
+    if((a=="") or (b=="")):
+        break
+    a = int(a)
+    b = int(b)
+    if((a<=n) and (b<=n) and (a>0) and (b>0)):
+        G.AdicionarAresta(a,b)
+for (u,v) in G.E():
+    print((u,v))
